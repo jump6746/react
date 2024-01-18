863382b43eb4d2f83233e40ef026c93cc5aa480d
jump6746 <jump6746@naver.com>
2024-01-18 13:44:46 +0900

    [스타일링] - main.css

---
+++ lecture/04-hello-react.js
@@ -0,0 +1,36 @@
+import { createElement as h /* hyperScript */ } from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom";
+
+// React.createElement
+// ReactDom.createRoot
+
+const strongElement = h("strong", {}, "fundamental");
+
+const h1Element = h(
+  "h1",
+  {},
+  "안녕!",
+  h("br"),
+  "리액트."
+);
+
+const pElement = h(
+  "p",
+  {},
+  "리액트는 UI 구축을 위한 자바스크립트 라이브러리입니다."
+);
+
+const appElement = h(
+  "div",
+  {id: "app"},
+  strongElement,
+  h1Element,
+  pElement,
+);
+
+const reactDomRoot = createRoot(
+  document.getElementById("root")
+);
+
+reactDomRoot.render(appElement);
+
